a547c79a6eeb6f8b9b279cc1df2b18468e6eed96
Nidhi Tandon <nidhitandon08@gmail.com>
2022-08-31 11:05:30 +0530

    feat: fetch movie info from omdb api

--- functions/movies.js
+++ functions/movies.js
@@ -1,8 +1,31 @@
+const {URL} = require('url');
+const fetch = require("node-fetch");
 const movies = require('../data/movies.json');
+require("dotenv").config();
 
-exports.handler = async () =>{
-    return{
+// http://www.omdbapi.com/?i=tt3896198&apikey=eb504316
+
+exports.handler = async () => {
+    const api = new URL('https://www.omdbapi.com/');
+    api.searchParams.set('apikey', process.env.OMDB_API_KEY);
+
+    const promises = movies.map((movie) => {
+        api.searchParams.set('i', movie.id);
+        return fetch(api).then((res) => res.json())
+            .then((data) => {
+            console.log("data", data)
+            const scores = data.Ratings;
+            return {
+                ...movie,
+                scores
+            }
+        })
+    })
+
+    const movieWithRatings = await Promise.all(promises);
+
+    return {
         statusCode: 200,
-        body: JSON.stringify(movies)
+        body: JSON.stringify(movieWithRatings)
     }
 }
